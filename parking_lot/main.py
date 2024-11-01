c9030fb31ffbef99a99d96bf125ef02456a7432e
Luis Felipe Sales Bignotto <luisfelipe.bignotto@gmail.com>
2024-11-01 19:57:47 -0300

    arrumado as rotas

--- parking_lot/main.py
+++ parking_lot/main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, UploadFile, File, Request
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from detector import ParkingDetector
 import uvicorn
 import cv2
+import numpy as np
+import io
+import detector
 
 app = FastAPI()
-detector = ParkingDetector()
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,39 +17,54 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/", StaticFiles(directory="./", html=True), name="html")
+detector_instance = detector.ParkingDetector()
 
-@app.get("/camera_image")
-async def camera_image():
-    frame = detector.get_current_frame()
-    if frame is None:
-        return JSONResponse(content={"error": "Não foi possível capturar a imagem da câmera."}, status_code=500)
-    _, img_encoded = cv2.imencode('.jpg', frame)
-    return Response(content=img_encoded.tobytes(), media_type="image/jpeg")
+@app.get("/")
+async def get():
+    return FileResponse("index.html")
+
+@app.post("/upload_image")
+async def upload_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite("uploaded_image.jpg", image)
+    return {"message": "Imagem recebida com sucesso."}
 
 @app.post("/update_coordinates")
 async def update_coordinates(request: Request):
     data = await request.json()
-    detector.update_coordinates(data)
+    detector_instance.update_coordinates(data["coordinates"])
     return {"status": "success"}
 
-@app.get("/video_feed")
-def video_feed():
-    return StreamingResponse(detector.video_generator(), media_type='multipart/x-mixed-replace; boundary=frame')
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    detector_instance.start_detection()
+    try:
+        while True:
+            try:
+                # Recebe e processa a imagem
+                message = await websocket.receive()
+                if message.get("type") == "websocket.disconnect":
+                    print("Cliente desconectado")
+                    break
+                elif "bytes" in message:
+                    frame_bytes = message["bytes"]
+                    nparr = np.frombuffer(frame_bytes, np.uint8)
+                    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-@app.get("/status")
-async def get_status():
-    status = detector.get_status()
-    return JSONResponse(content=status)
+                    processed_frame = detector_instance.detect_motion(frame)
+                    _, buffer = cv2.imencode('.jpg', processed_frame)
 
-@app.on_event("startup")
-async def startup_event():
-    detector.start_detection()
-
-@app.on_event("shutdown")
-def shutdown_event():
-    detector.stop_detection()
+                    await websocket.send_bytes(buffer.tobytes())
+                else:
+                    print("Mensagem não reconhecida:", message)
+            except WebSocketDisconnect:
+                print("Desconexão detectada no WebSocket")
+                break
+    finally:
+        detector_instance.stop_detection()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
-    
+    uvicorn.run(app, host="0.0.0.0", port=8000)
